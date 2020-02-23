575baae4b101b829c46429095d9b59b7efc3ef0b
Igor Canko Minotto <igorcankominotto@gmail.com>
2020-02-23 20:09:46 -0300

    Note API

--- nestjs/src/note/note.controller.ts
+++ nestjs/src/note/note.controller.ts
@@ -1,18 +1,38 @@
-import { Controller, Body, Post, Get } from '@nestjs/common';
+import { Controller, Body, Post, Get, Param, Put, Delete } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { NoteDTO } from './dto/note.dto';
+import { Note } from './note.entity';
 
 @Controller('note')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
   @Post()
-  public create(@Body() note: NoteDTO) {
-    this.noteService.create(note);
+  public async create(@Body() note: NoteDTO): Promise<Note> {
+    return this.noteService.create(note);
+  }
+
+  @Put('/:id')
+  public async update(@Param('id') id: number, @Body() noteDTO: Partial<NoteDTO>): Promise<Note> {
+    const note = await this.noteService.findOne(id);
+  
+    return this.noteService.update(note, noteDTO);
   }
 
   @Get()
-  public findAll(): NoteDTO[] {
+  public async findAll(): Promise<Note[]> {
     return this.noteService.findAll();
+  }
+
+  @Get('/:id')
+  public async find(@Param('id') id: number): Promise<Note> {
+    return this.noteService.findOne(id);
+  }
+
+  @Delete('/:id')
+  public async delete(@Param('id') id: number): Promise<void> {
+    const note = await this.noteService.findOne(id);
+
+    note.remove();
   }
 }
--- nestjs/src/note/note.service.ts
+++ nestjs/src/note/note.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { NoteDTO } from './dto/note.dto';
+import { Note } from './note.entity';
 
 @Injectable()
 export class NoteService {
-  private readonly notes: NoteDTO[] = [];
-
-  public create(note: NoteDTO) {
-    this.notes.push(note);
+  public async create(noteDTO: NoteDTO): Promise<Note> {
+    return Note.create(noteDTO).save();
   }
 
-  public findAll(): NoteDTO[] {
-    return this.notes;
+  public async update(note: Note, noteDTO: Partial<NoteDTO>): Promise<Note> {
+    return Note.merge(note, noteDTO).save();
+  }
+
+  public async findOne(id: number): Promise<Note> {
+    const note = await Note.findOne(id);
+
+    if (!note) throw new NotFoundException();
+
+    return note;
+  }
+
+  public async findAll(): Promise<Note[]> {
+    return Note.find();
   }
 }
