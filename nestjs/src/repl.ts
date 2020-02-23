934ad24da8c877ed5c16f6eda59dc1bfe7a88cb4
Igor Canko Minotto <igorcankominotto@gmail.com>
2020-02-23 13:06:40 -0300

    update repl

--- nestjs/src/repl.ts
+++ nestjs/src/repl.ts
@@ -1,18 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as colors from 'colors';
 import * as repl from 'repl';
-import { enable } from 'colors';
 
-function run() {
-    NestFactory.createApplicationContext(AppModule)
-        .then(app => {
-            enable();
-            const r = repl.start({
-              useColors: true,
-              prompt: 'nest.js >'.bgRed.bold + ' '
-            })
+(_ => {
+  const nestAppPromise = NestFactory.createApplicationContext(AppModule)
+  const replOptions = { 
+    prompt: ' nest.js '.bgBlue.bold + ''.blue + ' ',
+    useColors: true,
+  }
 
-            r.context.app = app
-        })
-}
-run()
+  nestAppPromise.then(app => {
+    const r = repl.start(replOptions)
+    
+    r.context.app = app
+    colors.enable();
+  })
+})()
