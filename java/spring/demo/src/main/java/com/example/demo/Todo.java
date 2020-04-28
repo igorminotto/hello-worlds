18293ea8a6e453e806b7162dd05c95a2717d1ab1
igorminotto <igorcankominotto@gmail.com>
2020-04-28 11:03:47 -0300

    java spring boot

---
+++ java/spring/demo/src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Todo {
+  public String title;
+  public String body;
+  private int x;
+
+  Todo(String title, String body, int x) {
+    this.title = title;
+    this.body = body;
+    this.x = x;
+  }
+}
